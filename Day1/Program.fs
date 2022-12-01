9b88db917204ce63dc51951c7891319eec6da45d
Luca Pangerc <pangerc@catfp.ch>
2022-12-01 16:27:03 +0100

    first steps with F3

--- Day1/Program.fs
+++ Day1/Program.fs
@@ -1,2 +1,12 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System.IO
+
+let readLines (filePath:string) = seq {
+    use sr = new StreamReader (filePath)
+    while not sr.EndOfStream do
+        yield sr.ReadLine ()
+}
+
+let inputs = readLines "/Users/lucapangerc/workspace/AoC2022/Day1/inputs/test_input.txt"
+let printSeq seq1 = Seq.iter (printf "%A ") seq1
+
+printSeq inputs
